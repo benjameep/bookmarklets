90897653557dc5fba2238f228f7a3f6bb885da9d
benjameep <14208829+benjameep@users.noreply.github.com>
2019-01-18 00:16:14 -0700

    getting closer...

--- build.js
+++ build.js
@@ -8,12 +8,13 @@ const template = read('src/template.ejs')
 const loader = read('src/loader.js')
 
 const bookmarklets = [{
+    title:'Quiz Save/Load',
     name:'quizsaver',
     url:'https://byui.instructure.com/files/6572492/download',
 }]
 
-console.log(bookmarklets.map(b => b.src=
+console.log(bookmarklets.map(b => b.src=encodeURIComponent(
     `${loader}("${b.name}","${b.url}",function(){window.${b.name}.default()})`
-))
+)))
 
 fs.writeFileSync(path.join(__dirname,'README.md'),ejs.render(template,{loader,bookmarklets}))
